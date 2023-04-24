041c1e42216693aaf67354adc97eb4924fdad9d6
Sergey Shmatok <trycifer@gmail.com>
2023-04-24 18:25:06 +0300

    Add FriendList component and styles

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import user from './components/profile/user.json';
 import { Statistics } from './components/statistics/Statistics';
 import data from './components/statistics/data.json';
 
+import { FriendList } from 'components/friend-list/FriendList';
+import friends from './components/friend-list/friends.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App>
@@ -19,8 +22,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </App>
   </React.StrictMode>
 );
