63b2f6355c43b16b2df61a247e1036e96b0e895b
Serhii Shmatok <48224169+Cryosphere@users.noreply.github.com>
2023-04-25 21:17:24 +0300

    Update TransactionHistory.jsx

--- src/components/transaction-history/TransactionHistory.jsx
+++ src/components/transaction-history/TransactionHistory.jsx
@@ -25,10 +25,10 @@ export const TransactionHistory = ({ items }) => (
 TransactionHistory.propTypes = {
   items: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.number.isRequired,
+      id: PropTypes.string.isRequired,
       type: PropTypes.string.isRequired,
       amount: PropTypes.number.isRequired,
-      currency: PropTypes.number.isRequired,
+      currency: PropTypes.string.isRequired,
     })
   ).isRequired,
 };
